ebff9f49ea0e447228159c53003c5bc5a17a0459
Ana Ordonez <anaordonez@comcast.net>
2016-11-25 10:30:56 -0700

    working regridding script

--- grid1togrid2.py
+++ grid1togrid2.py
@@ -1,3 +1,6 @@
+import numpy as np
+from netCDF4 import Dataset
+
 def grid1togrid2(ongrid1,ncfile):
     """
     ncfile = '/glade/p/work/aordonez/cesm_mapping/map_gx1v6NH_TO_stereo25km_blin.161123.nc'
@@ -8,7 +11,6 @@ def grid1togrid2(ongrid1,ncfile):
 
     griddata = Dataset(ncfile)
     N=griddata.variables['dst_grid_dims'][:]
-    Ntot=N[0]*N[1]
 
     # nasa stereo gridcell location
     dst=griddata.variables['row'][:] 
@@ -23,29 +25,29 @@ def grid1togrid2(ongrid1,ncfile):
     src = src - 1
 
     NN=ongrid1.shape
-
-    if len(NN)<3: # just 2 D input 
-        # weights are upside-down relative to how python reads netcdf
-        tmp = ongrid1.flatten()
-        tmp = np.reshape(tmp,(len(tmp),1))
-        ongrid2=np.zeros((Ntot,1))
-        # multiply each element of the matrix (not matrix multiplication)
-        for ind in np.unique(dst):
-            k = np.where(dst == ind)
-            totwt = np.sum(S[k])
-            ongrid2[ind]=np.sum(S[k]*tmp[src[k]]) / totwt
-        ongrid2=np.reshape(ongrid2,(N[1],N[0]))
-        f,axs = plt.subplots(2,1)
-        axs[0].pcolormesh(np.reshape(tmp,(NN[0],NN[1])))
-        axs[1].pcolormesh(ongrid2)
-        f.show()
+    
+    if len(NN) < 3:
+        ongrid2 = regrid_timestep(ongrid1,N,dst,src,S)
     else:
-        ongrid2=np.zeros((NN[0],Ntot))
-        for n=1:NN[0]:
-            ongrid2[n,dst]=S*ongrid1[n,src]
-        end
-        ongrid2=ongrid2/(np.ones((NN([0],1))*totwts)
-        ongrid2=np.reshape(ongrid2,(NN[0],N[0],N[1])) 
-        ongrid2=np.transpose(ongrid2,(0 2 1)) 
+       ongrid2 = np.zeros((ongrid1.shape[0],N[1],N[0]))
+       for t in range(0,ongrid1.shape[0]):
+            ongrid2[t,:,:] = regrid_timestep(ongrid1,N,dst,src,S)
+
+    return ongrid2
+
+def regrid_timestep(ongrid1, N, dst, src, S):
+    Ntot=N[0]*N[1]
+    # weights are upside-down relative to how python reads netcdf
+    tmp = ongrid1.flatten()
+    tmp = np.reshape(tmp,(len(tmp),1))
+    ongrid2=np.zeros((Ntot,1))
+    # multiply each element of the matrix (not matrix multiplication)
+    for ind in np.unique(dst):
+        k = np.where(dst == ind)
+        totwt = np.sum(S[k])
+        ongrid2[ind]=np.sum(S[k]*tmp[src[k]]) / totwt
+    ongrid2=np.reshape(ongrid2,(N[1],N[0]))
+    return ongrid2
+
 
 
