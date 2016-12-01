f068742bbb97fcd0e1ff37370b1652d42c75e355
Ana Ordonez <anaordonez@comcast.net>
2016-12-01 10:58:35 -0700

    added temperature

--- make_storm_composite_plots.py
+++ make_storm_composite_plots.py
@@ -4,16 +4,19 @@ import matplotlib.pyplot as plt
 
 pproj = np.load('/glade/scratch/aordonez/pproj.npy')
 iproj = np.load('/glade/scratch/aordonez/iproj.npy')
+tproj = np.load('/glade/scratch/aordonez/tproj.npy')
 
 lat,lon = read_stereo_lat_lon()
 
 pproj = pproj[0:(365*10),:,:]
 iproj = iproj[0:(365*10),:,:]
+tproj = tproj[0:(365*10),:,:]
 
 lows = find_cyclone_center(pproj,iproj,98000,90000)
 if np.max(lows) == 1.0:
-    box = get_boxes(lows,pproj,50,lon) 
+    box = get_boxes(lows,pproj,50,lon,90000) 
     box[box == 0.0] = np.nan
+    tbox = get_boxes(lows,tproj,50,lon,200)
 else:
     print "Max of lows is ",np.max(lows)
     print "Check that pmax and pmin are in correct order"
