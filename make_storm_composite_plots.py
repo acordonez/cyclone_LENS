cc346f41d0eb36d3c78cb4525c804fb1d5ee6802
Ana Ordonez <anaordonez@comcast.net>
2016-12-01 15:39:11 -0700

    changed file to load from

--- make_storm_composite_plots.py
+++ make_storm_composite_plots.py
@@ -2,21 +2,19 @@ from import_LE_data import *
 from cyclone_composite_LENS import *
 import matplotlib.pyplot as plt
 
-pproj = np.load('/glade/scratch/aordonez/pproj.npy')
-iproj = np.load('/glade/scratch/aordonez/iproj.npy')
-tproj = np.load('/glade/scratch/aordonez/tproj.npy')
+pproj = np.load('/glade/scratch/aordonez/pproj_small.npy')
+iproj = np.load('/glade/scratch/aordonez/iproj_small.npy')
+#tproj = np.load('/glade/scratch/aordonez/tproj.npy')
 
 lat,lon = read_stereo_lat_lon()
 
-pproj = pproj[0:(365*10),:,:]
-iproj = iproj[0:(365*10),:,:]
-tproj = tproj[0:(365*10),:,:]
+#tproj = tproj[0:(365*10),:,:]
 
 lows = find_cyclone_center(pproj,iproj,98000,90000)
 if np.max(lows) == 1.0:
     box = get_boxes(lows,pproj,50,lon,90000) 
     box[box == 0.0] = np.nan
-    tbox = get_boxes(lows,tproj,50,lon,200)
+    #tbox = get_boxes(lows,tproj,50,lon,200)
 else:
     print "Max of lows is ",np.max(lows)
     print "Check that pmax and pmin are in correct order"
