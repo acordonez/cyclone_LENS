25dfbde84b0608caa36d22ae754c6a208b3cb52b
Ana Ordonez <anaordonez@comcast.net>
2016-12-02 19:28:18 -0700

    added working sanity checks; better composites now

--- make_storm_composite_plots.py
+++ make_storm_composite_plots.py
@@ -2,29 +2,39 @@ from import_LE_data import *
 from cyclone_composite_LENS import *
 import matplotlib.pyplot as plt
 
-pproj = np.load('/glade/scratch/aordonez/pproj_small.npy')
-iproj = np.load('/glade/scratch/aordonez/iproj_small.npy')
-#tproj = np.load('/glade/scratch/aordonez/tproj.npy')
+pproj = np.load('/glade/scratch/aordonez/pproj.npy')
+iproj = np.load('/glade/scratch/aordonez/iproj.npy')
+tproj = np.load('/glade/scratch/aordonez/tproj.npy')
+tproj = tproj[0:3650 * 3,:,:]
+iproj = iproj[0:3650 * 3,:,:]
+pproj = pproj[0:3650 * 3,:,:]
 
 lat,lon = read_stereo_lat_lon()
 
-#tproj = tproj[0:(365*10),:,:]
-
-lows = find_cyclone_center(pproj,iproj,98000,90000)
+lows = find_cyclone_center(pproj,iproj,104000,90000)
 if np.max(lows) == 1.0:
-    box = get_boxes(lows,pproj,50,lon,90000) 
+    box,_,_ = get_boxes(lows,pproj,50,lat,lon,90000) 
+    # replace no data and absurd values with nan:
     box[box == 0.0] = np.nan
-    #tbox = get_boxes(lows,tproj,50,lon,200)
+    box[box > 108000] = np.nan   
+    box[box < 90000] = np.nan
+
+    tbox,_,_=get_boxes(lows,tproj,50,lat,lon,90000)
+    tbox[tbox == 0.0] = np.nan
+    tbox[tbox < 180.] = np.nan
+    tbox[tbox > 360.] = np.nan
+
+    # remove samples where SLP at 'low' is greater than box average:
+    pmean = np.nanmean(np.nanmean(box,axis = 2),axis = 1)
+    k = np.where(box[:,50,50] < pmean)
+    box = box[k[0],:,:]
+    tbox = tbox[k[0],:,:]
+
+    plt.pcolormesh(np.nanmean(tbox,axis = 0))
+    plt.contour(np.nanmean(box,axis = 0),range(96000,103000,100))
+    
 else:
     print "Max of lows is ",np.max(lows)
     print "Check that pmax and pmin are in correct order"
 
-plot_mean(box)
 
-20yrs = 20 * 365 
-n = 5
-
-for era in range(0,n):
-    start = era * 20yrs
-    end = (era + 1) * 20yrs
-    boxes = box[start:end,:,:]
