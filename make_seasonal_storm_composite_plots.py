c1416b0d7c4f8ac967967bf69134500fd04a1bb9
Ana Ordonez <anaordonez@comcast.net>
2016-12-05 11:52:09 -0700

    scripts save test figures

---
+++ make_seasonal_storm_composite_plots.py
@@ -0,0 +1,56 @@
+from import_LE_data import *
+from cyclone_composite_LENS import *
+import matplotlib.pyplot as plt
+
+print 'loading data'
+pproj = np.load('/glade/scratch/aordonez/pproj.npy')
+iproj = np.load('/glade/scratch/aordonez/iproj.npy')
+tproj = np.load('/glade/scratch/aordonez/tproj.npy')    
+lat,lon = read_stereo_lat_lon()
+
+seasons = ['djf','mam','jja','son']
+season_functions = {'djf':get_djf,'mam':get_mam,'jja':get_jja,'son':get_son}
+
+print 'making composites'
+for n in range(0,4):
+    print 'set = ', str(n)
+    start = 20*365*n 
+    end = start + (20*365)
+
+    tprojn = tproj[start:end,:,:]
+    iprojn = iproj[start:end,:,:]
+    pprojn = pproj[start:end,:,:]
+
+    for season in seasons:
+        get_season = season_functions[season]
+        pprojn = get_season(pprojn)
+        iprojn = get_season(iprojn)
+        tprojn = get_season(tprojn)
+        lows = find_cyclone_center(pprojn,iprojn,104000,90000)
+
+        if np.max(lows) == 1.0:
+            box,_,_ = get_boxes(lows,pprojn,50,lat,lon,90000) 
+            # replace no data and absurd values with nan:
+            box[box == 0.0] = np.nan
+            box[box > 108000] = np.nan   
+            box[box < 90000] = np.nan
+
+            tbox,_,_=get_boxes(lows,tprojn,50,lat,lon,90000)
+            tbox[tbox == 0.0] = np.nan
+            tbox[tbox < 180.] = np.nan
+            tbox[tbox > 360.] = np.nan
+
+            # remove samples where SLP at 'low' is greater than box average:
+            pmean = np.nanmean(np.nanmean(box,axis = 2),axis = 1)
+            k = np.where(box[:,50,50] < pmean)
+            box = box[k[0],:,:]
+            tbox = tbox[k[0],:,:]
+
+            f,ax = plt.subplots(1,1)
+            ax.pcolormesh(np.nanmean(tbox,axis = 0))
+            ax.contour(np.nanmean(box,axis = 0),range(96000,103000,100))
+            f.savefig('test_' + str(n) + '_' + season + 'png')
+    
+
+
+
--- make_storm_composite_plots.py
+++ make_storm_composite_plots.py
@@ -2,39 +2,46 @@ from import_LE_data import *
 from cyclone_composite_LENS import *
 import matplotlib.pyplot as plt
 
+print 'loading data'
 pproj = np.load('/glade/scratch/aordonez/pproj.npy')
 iproj = np.load('/glade/scratch/aordonez/iproj.npy')
-tproj = np.load('/glade/scratch/aordonez/tproj.npy')
-tproj = tproj[0:3650 * 3,:,:]
-iproj = iproj[0:3650 * 3,:,:]
-pproj = pproj[0:3650 * 3,:,:]
-
+tproj = np.load('/glade/scratch/aordonez/tproj.npy')    
 lat,lon = read_stereo_lat_lon()
 
-lows = find_cyclone_center(pproj,iproj,104000,90000)
-if np.max(lows) == 1.0:
-    box,_,_ = get_boxes(lows,pproj,50,lat,lon,90000) 
-    # replace no data and absurd values with nan:
-    box[box == 0.0] = np.nan
-    box[box > 108000] = np.nan   
-    box[box < 90000] = np.nan
+print 'making composites'
+for n in range(0,4):
+    print 'set = ', str(n)
+    start = 20*365*n 
+    end = start + (20*365)
 
-    tbox,_,_=get_boxes(lows,tproj,50,lat,lon,90000)
-    tbox[tbox == 0.0] = np.nan
-    tbox[tbox < 180.] = np.nan
-    tbox[tbox > 360.] = np.nan
+    tprojn = tproj[start:end,:,:]
+    iprojn = iproj[start:end,:,:]
+    pprojn = pproj[start:end,:,:]
 
-    # remove samples where SLP at 'low' is greater than box average:
-    pmean = np.nanmean(np.nanmean(box,axis = 2),axis = 1)
-    k = np.where(box[:,50,50] < pmean)
-    box = box[k[0],:,:]
-    tbox = tbox[k[0],:,:]
+    lows = find_cyclone_center(pprojn,iprojn,104000,90000)
+    if np.max(lows) == 1.0:
+        box,_,_ = get_boxes(lows,pprojn,50,lat,lon,90000) 
+        # replace no data and absurd values with nan:
+        box[box == 0.0] = np.nan
+        box[box > 108000] = np.nan   
+        box[box < 90000] = np.nan
 
-    plt.pcolormesh(np.nanmean(tbox,axis = 0))
-    plt.contour(np.nanmean(box,axis = 0),range(96000,103000,100))
+        tbox,_,_=get_boxes(lows,tprojn,50,lat,lon,90000)
+        tbox[tbox == 0.0] = np.nan
+        tbox[tbox < 180.] = np.nan
+        tbox[tbox > 360.] = np.nan
+
+        # remove samples where SLP at 'low' is greater than box average:
+        pmean = np.nanmean(np.nanmean(box,axis = 2),axis = 1)
+        k = np.where(box[:,50,50] < pmean)
+        box = box[k[0],:,:]
+        tbox = tbox[k[0],:,:]
+
+        f,ax = plt.subplots(1,1)
+        ax.pcolormesh(np.nanmean(tbox,axis = 0))
+        ax.contour(np.nanmean(box,axis = 0),range(96000,103000,100))
+        f.savefig('test_' + str(n)+'.png')
     
-else:
-    print "Max of lows is ",np.max(lows)
-    print "Check that pmax and pmin are in correct order"
+
 
 
