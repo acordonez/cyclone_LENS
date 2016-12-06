c463d19b19d09e7718e8c726cdff025c71e69eee
Ana Ordonez <anaordonez@comcast.net>
2016-12-06 09:58:08 -0700

    fixing bugs

--- make_seasonal_storm_composite_plots.py
+++ make_seasonal_storm_composite_plots.py
@@ -5,14 +5,16 @@ import matplotlib.pyplot as plt
 print 'loading data'
 pproj = np.load('/glade/scratch/aordonez/pproj.npy')
 iproj = np.load('/glade/scratch/aordonez/iproj.npy')
-tproj = np.load('/glade/scratch/aordonez/tproj.npy')    
+tproj = np.load('/glade/scratch/aordonez/tproj.npy') 
+uproj = np.load('/glade/scratch/aordonez/UBOTproj.npy')
+vproj = np.load('/glade/scratch/aordonez/VBOTproj.npy')    
 lat,lon = read_stereo_lat_lon()
 
 seasons = ['djf','mam','jja','son']
 season_functions = {'djf':get_djf,'mam':get_mam,'jja':get_jja,'son':get_son}
 
 print 'making composites'
-for n in range(0,4):
+for n in range(0,5):
     print 'set = ', str(n)
     start = 20*365*n 
     end = start + (20*365)
@@ -20,35 +22,60 @@ for n in range(0,4):
     tprojn = tproj[start:end,:,:]
     iprojn = iproj[start:end,:,:]
     pprojn = pproj[start:end,:,:]
+    uprojn = uproj[start:end,:,:]
+    vprojn = vproj[start:end,:,:]
 
     for season in seasons:
         get_season = season_functions[season]
-        pprojn = get_season(pprojn)
-        iprojn = get_season(iprojn)
-        tprojn = get_season(tprojn)
-        lows = find_cyclone_center(pprojn,iprojn,104000,90000)
-
-        if np.max(lows) == 1.0:
-            box,_,_ = get_boxes(lows,pprojn,50,lat,lon,90000) 
+        pprojseas = get_season(pprojn)
+        iprojseas = get_season(iprojn)
+        tprojseas = get_season(tprojn)
+        uprojseas = get_season(uprojn)
+        vprojseas = get_season(vprojn)
+        lows = find_cyclone_center(pprojseas,iprojseas,104000,90000)
+        print lows.shape
+        if (len(lows) < 1):
+            continue
+        elif (np.max(lows) == 1.0):
+            box,_,_ = get_boxes(lows,pprojseas,50,lat,lon,90000) 
             # replace no data and absurd values with nan:
             box[box == 0.0] = np.nan
             box[box > 108000] = np.nan   
             box[box < 90000] = np.nan
 
-            tbox,_,_=get_boxes(lows,tprojn,50,lat,lon,90000)
+            tbox,_,_=get_boxes(lows,tprojseas,50,lat,lon,90000)
             tbox[tbox == 0.0] = np.nan
             tbox[tbox < 180.] = np.nan
             tbox[tbox > 360.] = np.nan
+
+            ibox,_,_  = get_boxes(lows,iprojseas,50,lat,lon,10)
+            ibox[ibox <= 0.0] = np.nan
+            ibox[ibox > 110] = np.nan
+
+            ubox,_,_ = get_boxes(lows,uprojseas,50,lat,lon,500)
+            vbox,_,_ = get_boxes(lows,vprojseas,50,lat,lon,500)
+            ubox[ubox == 0.0] = np.nan
+            vbox[vbox == 0.0] = np.nan
+            ubox[abs(ubox) > 500] = np.nan
+            vbox[abs(vbox) > 500] = np.nan
 
             # remove samples where SLP at 'low' is greater than box average:
             pmean = np.nanmean(np.nanmean(box,axis = 2),axis = 1)
             k = np.where(box[:,50,50] < pmean)
             box = box[k[0],:,:]
             tbox = tbox[k[0],:,:]
+            ibox = ibox[k[0],:,:]
+            ubox = ubox[k[0],:,:]
+            vbox = vbox[k[0],:,:]
 
-            f,ax = plt.subplots(1,1)
-            ax.pcolormesh(np.nanmean(tbox,axis = 0))
-            ax.contour(np.nanmean(box,axis = 0),range(96000,103000,100))
+            X,Y = np.meshgrid(range(0,50),range(0,50))
+
+            f,axs = plt.subplots(1,1)
+            h = axs.pcolormesh(X,Y,np.nanmean(ibox,axis = 0),vmin = 0, vmax = 100)
+            axs.streamplot(X,Y,Umean,Vmean,linewidth = 1)
+            axs.contour(X,Y,np.nanmean(tbox,axis = 0),range(240,300,5))
+            axs.contour(X,Y,np.nanmean(box,axis = 0),range(96000,103000,100))
+            f.colorbar(h,ax = axs)
             f.savefig('test_' + str(n) + '_' + season + 'png')
     
 
--- make_storm_composite_plots.py
+++ make_storm_composite_plots.py
@@ -2,14 +2,19 @@ from import_LE_data import *
 from cyclone_composite_LENS import *
 import matplotlib.pyplot as plt
 
+# load data on NH EASE stereo grid
 print 'loading data'
 pproj = np.load('/glade/scratch/aordonez/pproj.npy')
 iproj = np.load('/glade/scratch/aordonez/iproj.npy')
-tproj = np.load('/glade/scratch/aordonez/tproj.npy')    
+tproj = np.load('/glade/scratch/aordonez/tproj.npy')  
+uproj = np.load('/glade/scratch/aordonez/UBOTproj.npy')
+vproj = np.load('/glade/scratch/aordonez/VBOTproj.npy')
 lat,lon = read_stereo_lat_lon()
 
 print 'making composites'
-for n in range(0,4):
+for n in range(0,5):
+    # make composite plot for each 20 year period
+    # from 1980 - 2080
     print 'set = ', str(n)
     start = 20*365*n 
     end = start + (20*365)
@@ -17,7 +22,9 @@ for n in range(0,4):
     tprojn = tproj[start:end,:,:]
     iprojn = iproj[start:end,:,:]
     pprojn = pproj[start:end,:,:]
-
+    uprojn = uproj[start:end,:,:]
+    vprojn = vproj[start:end,:,:]
+ 
     lows = find_cyclone_center(pprojn,iprojn,104000,90000)
     if np.max(lows) == 1.0:
         box,_,_ = get_boxes(lows,pprojn,50,lat,lon,90000) 
@@ -26,20 +33,39 @@ for n in range(0,4):
         box[box > 108000] = np.nan   
         box[box < 90000] = np.nan
 
-        tbox,_,_=get_boxes(lows,tprojn,50,lat,lon,90000)
+        tbox,_,_=get_boxes(lows,tprojn,50,lat,lon,300)
         tbox[tbox == 0.0] = np.nan
         tbox[tbox < 180.] = np.nan
         tbox[tbox > 360.] = np.nan
+ 
+        ibox,_,_  = get_boxes(lows,iprojn,50,lat,lon,10)
+        ibox[ibox <= 0.0] = np.nan
+        ibox[ibox > 110] = np.nan
+
+        ubox,_,_ = get_boxes(lows,uprojn,50,lat,lon,500)
+        vbox,_,_ = get_boxes(lows,vprojn,50,lat,lon,500)
+        ubox[ubox == 0.0] = np.nan
+        vbox[vbox == 0.0] = np.nan
+        ubox[abs(ubox) > 500] = np.nan
+        vbox[abs(vbox) > 500] = np.nan
 
         # remove samples where SLP at 'low' is greater than box average:
         pmean = np.nanmean(np.nanmean(box,axis = 2),axis = 1)
         k = np.where(box[:,50,50] < pmean)
         box = box[k[0],:,:]
         tbox = tbox[k[0],:,:]
+        ibox = ibox[k[0],:,:]
+        ubox = ubox[k[0],:,:]
+        vbox = vbox[k[0],:,:]
 
-        f,ax = plt.subplots(1,1)
-        ax.pcolormesh(np.nanmean(tbox,axis = 0))
-        ax.contour(np.nanmean(box,axis = 0),range(96000,103000,100))
+        X,Y = np.meshgrid(range(0,50),range(0,50))
+
+        f,axs = plt.subplots(1,1)
+        h = axs.pcolormesh(X,Y,np.nanmean(ibox,axis = 0),vmin = 0, vmax = 100)
+        axs.streamplot(X,Y,ubox,vbox,linewidth = 1)
+        axs.contour(X,Y,np.nanmean(tbox,axis = 0),range(240,300,5))
+        axs.contour(X,Y,np.nanmean(box,axis = 0),range(96000,103000,100))
+        f.colorbar(h,ax = axs)
         f.savefig('test_' + str(n)+'.png')
     
 
