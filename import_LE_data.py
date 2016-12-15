bfa3f7ed9dacf83227e447726c815d703026710d
Ana Ordonez <anaordonez@comcast.net>
2016-12-15 12:54:44 -0700

    added SH functions

--- import_LE_data.py
+++ import_LE_data.py
@@ -109,6 +109,59 @@ def read_atm_data(varname,num):
     var = np.concatenate((var,var3),axis = 0)
     return var
 
+def read_ocn_data(varname,num):
+    """read_ocn_data(varname,num)
+    varname: string
+        name of the variable to read from file
+    num: string
+        ID number of the ensemble member to read
+    var: numpy array
+        3-D array of variable from 1980-2100 in NH
+    """
+
+    if len(num) < 3:
+        num = num.zfill(3)
+
+    fdir = ('/glade/p/cesm0005/CESM-CAM5-BGC-LE/ocn/proc/tseries/daily/' 
+           + varname + '/')
+    if num == '001':
+        t1 = ((1980-1850)*365) - 1
+        fname1 = (fdir + 'b.e11.B20TRC5CNBDRD.f09_g16.' 
+                 + num + '.pop.h.nday1.' + varname
+                 + '.18500102-20051231.nc')
+    else:
+        t1 = ((1980-1920)*365) - 1
+        fname1 = (fdir + 'b.e11.B20TRC5CNBDRD.f09_g16.' 
+                 + num + '.pop.h.nday1.' + varname 
+                 + '.19200102-20051231.nc')
+
+    fname2 = (fdir + 'b.e11.BRCP85C5CNBDRD.f09_g16.' 
+             + num + '.pop.h.nday1.' + varname 
+             + '.20060102-20801231.nc')
+    fname3 = (fdir + 'b.e11.BRCP85C5CNBDRD.f09_g16.' 
+             + num + '.pop.h.nday1.' + varname 
+             + '.20810101-21001231.nc')
+
+    data = Dataset(fname1)
+    var1 = data.variables[varname][t1:,:,:]
+    data = Dataset(fname2)
+    var2 = data.variables[varname][:]
+    data = Dataset(fname3)
+    var3 = data.variables[varname][:(365*19),:,:]
+
+    var = np.concatenate((var1,var2), axis = 0)
+    var = np.concatenate((var,var3),axis = 0)
+    return var
+
+def read_native_lat_lon_atm():
+    fname = '/glade/p/cesm0005/CESM-CAM5-BGC-LE/atm/proc/tseries/daily/TS/b.e11.B20TRC5CNBDRD.f09_g16.001.cam.h1.TS.18500101-20051231.nc'
+    data = Dataset(fname)
+    lat = data['lat'][:]
+    lon = data['lon'][:]
+    lon,lat = np.meshgrid(lon,lat)
+    return lat, lon
+   
+
 def read_stereo_lat_lon():
     """read_stereo_lat_lon
     Returns the latitude and longitude grids for the 
@@ -141,7 +194,7 @@ def save_ice_vars_as_stereo():
     #varlist = {'aice':aice,'daidtd':daidtd,'daidtt':daidtt,
     #           'frazil':frazil,'congel':congel,'snoice':snoice,
     #           'melts':melts,'meltb':meltb,'meltt':meltt}
-    varlist = {'daidtd':daidtd,'daidtt':daidtt}
+    #varlist = {'daidtd':daidtd,'daidtt':daidtt}
     ncfile =  '/glade/p/work/aordonez/cesm_mapping/map_gx1v6NH_TO_stereo25km_blin.161123.nc'
     for varname in varlist:
         print "reading data from file"
@@ -154,6 +207,26 @@ def save_ice_vars_as_stereo():
         np.save('/glade/scratch/aordonez/'+varname+'proj.npy',tmp)
     return varlist
 
+def save_ocn_vars_as_stereo():
+    """save_ocn_vars_as_stereo
+    Regrids ocn data from gx1v6 native grid
+    to NH stereo polar grid and saves as .npy
+    """
+    SST = np.zeros((1,1))
+    varlist = {'SST':SST}
+    ncfile =  '/glade/p/work/aordonez/cesm_mapping/map_gx1v6NH_TO_stereo25km_blin.161123.nc'
+    for varname in varlist:
+        print "reading data from file"
+        var = read_ocn_data(varname,'001')
+        print "regridding to stereo"
+        tmp= grid1togrid2(var,ncfile)
+        print "reshaping"
+        tmp = np.transpose(tmp,(2,0,1))
+        varlist[varname] = tmp
+        np.save('/glade/scratch/aordonez/'+varname+'proj.npy',tmp)
+    return varlist
+
+
 def save_atm_vars_as_stereo():
     """save_atm_vars_as_stereo
     Regrids atmosphere data from fv0.9x1.25 native grid
@@ -161,11 +234,11 @@ def save_atm_vars_as_stereo():
     """
     TS = np.zeros((1,1))
     PSL = np.zeros((1,1))
-    TAUX = np.zeros((1,1,))
-    TAUY = np.zeros((1,1))
+    UBOT = np.zeros((1,1,))
+    VBOT = np.zeros((1,1))
     ICE = np.zeros((1,1))
     #varlist = {'TS':TS,'PSL':PSL,'TAUX':TAUX,'TAUY':TAUY}
-    varlist = {'TS':TS}
+    varlist = {'UBOT':UBOT,'VBOT':VBOT}
     ncfile = '/glade/p/work/aordonez/cesm_mapping/map_fv0.9x1.25_TO_stereo25km_blin.161123.nc'
     for varname in varlist:
         print "reading data from file"
@@ -175,8 +248,106 @@ def save_atm_vars_as_stereo():
         print "reshaping"
         tmp = np.transpose(tmp,(2,0,1))
         varlist[varname] = tmp
+        np.save('/glade/scratch/aordonez/'+varname+'proj.npy',tmp)
     return varlist
 
+"""
+SOUTHERN HEMISPHERE FUNCTIONS
+"""
+
+def read_ice_data_SH(varname,num):
+    """read_ice_data(varname,num)
+    varname: string
+        name of the variable to read from file
+    num: string
+        ID number of the ensemble member to read
+    var: numpy array
+        3-D array of variable from 1980-2100 in NH
+    """
+
+    if len(num) < 3:
+        num = num.zfill(3)
+
+    fdir = ('/glade/p/cesm0005/CESM-CAM5-BGC-LE/ice/proc/tseries/daily/' + 
+           varname + '_d/')
+    if num == '001':
+        t1 = ((1980-1850)*365) - 1
+        fname1 = (fdir + 'b.e11.B20TRC5CNBDRD.f09_g16.' +
+                 num + '.cice.h1.
 
 
+def save_ice_vars_as_SHstereo():
+    """save_ice_vars_as_stereo
+    Regrids ice data from gx1v6 native grid
+    to SH stereo polar grid and saves as .npy
+    """
+    aice = np.zeros((1,1))
+    daidtd = np.zeros((1,1))
+    daidtt = np.zeros((1,1))
+    frazil = np.zeros((1,1))
+    congel = np.zeros((1,1)) 
+    snoice = np.zeros((1,1))
+    melts = np.zeros((1,1))
+    meltb = np.zeros((1,1))
+    meltt = np.zeros((1,1))
+    SST = np.zeros((1,1))
+    #varlist = {'aice':aice,'daidtd':daidtd,'daidtt':daidtt,
+    #           'frazil':frazil,'congel':congel,'snoice':snoice,
+    #           'melts':melts,'meltb':meltb,'meltt':meltt}
+    #varlist = {'aice':aice,'daidtd':daidtd,'daidtt':daidtt}
+    varlist = {'SST':SST}
+    ncfile =  '/glade/p/work/aordonez/cesm_mapping/map_gx1v6SH_TO_SHstereo25km_blin.161213.nc'
+    for varname in varlist:
+        print "reading data from file"
+        var = read_ice_data_SH(varname,'001')
+        print "regridding to stereo"
+        tmp= grid1togrid2(var,ncfile)
+        print "reshaping"
+        tmp = np.transpose(tmp,(2,0,1))
+        varlist[varname] = tmp
+        np.save('/glade/scratch/aordonez/'+varname+'SHproj.npy',tmp)
+    return varlist
+
+def save_ocn_vars_as_SHstereo():
+    """save_ocn_vars_as_stereo
+    Regrids ocn data from gx1v6 native grid
+    to SH stereo polar grid and saves as .npy
+    """
+    SST = np.zeros((1,1))
+    varlist = {'SST':SST}
+    ncfile =  '/glade/p/work/aordonez/cesm_mapping/map_gx1v6SH_TO_SHstereo25km_blin.161213.nc'
+    for varname in varlist:
+        print "reading data from file"
+        var = read_ocn_data(varname,'001')
+        print "regridding to stereo"
+        tmp= grid1togrid2(var,ncfile)
+        print "reshaping"
+        tmp = np.transpose(tmp,(2,0,1))
+        varlist[varname] = tmp
+        np.save('/glade/scratch/aordonez/'+varname+'SHproj.npy',tmp)
+    return varlist
+
+def save_atm_vars_as_SHstereo():
+    """save_atm_vars_as_stereo
+    Regrids atmosphere data from fv0.9x1.25 native grid
+    to SH stereo polar grid and saves as .npy
+    """
+    TS = np.zeros((1,1))
+    PSL = np.zeros((1,1))
+    UBOT = np.zeros((1,1,))
+    VBOT = np.zeros((1,1))
+    ICE = np.zeros((1,1))
+    #varlist = {'TS':TS,'PSL':PSL,'UBOT':UBOT,'VBOT':VBOT}
+    varlist = {'ICEFRAC':ICE}
+    ncfile = '/glade/p/work/aordonez/cesm_mapping/map_fv0.9x1.25_TO_SHstereo25km_blin.161213.nc'
+    for varname in varlist:
+        print "reading data from file"
+        var = read_atm_data(varname,'001')
+        print "regridding to stereo"
+        tmp = grid1togrid2(var,ncfile)
+        print "reshaping"
+        tmp = np.transpose(tmp,(2,0,1))
+        varlist[varname] = tmp
+        np.save('/glade/scratch/aordonez/'+varname+'SHproj.npy',tmp)
+    return varlist
 
