9cf868b8d24e7c2c6c713bdefa40e3a9312eb7ea
Ana Ordonez <anaordonez@comcast.net>
2017-02-03 15:12:16 -0700

    change to September

--- area_vs_extent_plots.py
+++ area_vs_extent_plots.py
@@ -12,9 +12,11 @@ import matplotlib.gridspec as gridspec
 
 area = read_area_ice()
 num_list = [str(x) for x in range(1,31)]
-ratio = np.zeros((30,1453))
+ratio = np.zeros((30,121))
 for ind,model_num in enumerate(num_list):
     aice = read_ice_data_monthly('aice',model_num)
+    time = aice.shape[0]
+    aice = aice[range(8,time,12),:,:]
     # remove fill values and mask
     aice[aice > 110] = 0
     aice = aice.data
@@ -26,24 +28,29 @@ for ind,model_num in enumerate(num_list):
     ratio[ind,:] = ice_area / ice_extent
 
 # plot annual ratios with +- 2 std (based on 1980-1999)
-ratio_ann = np.reshape(ratio[:,0:1452],(30,121,12))
+"""ratio_ann = np.reshape(ratio[:,0:1452],(30,121,12))
 ratio_ann = np.mean(ratio_ann,axis = 2)
 ratio_std = np.std(ratio_ann[:,0:30].flatten()) * 2.
 ratio_mean = np.nanmean(ratio_ann[:,0:30].flatten())
 ratio_ann = np.transpose(ratio_ann)
+"""
+ratio_ann = ratio[:,0:120]
+ratio_std = np.std(ratio_ann[:,0:30].flatten()) * 2
+ratio_mean = np.nanmean(ratio_ann[:,0:30].flatten())
+ratio_ann = np.transpose(ratio_ann)
 
-years = np.transpose(np.tile(np.arange(1980,2101),(30,1)))
+years = np.transpose(np.tile(np.arange(1980,2100),(30,1)))
 
 #1980-2100 subplot
 gs = gridspec.GridSpec(2,2)
 ax1 = plt.subplot(gs[0,:])
-ax1.set_autoscale_on(False)
+#ax1.set_autoscale_on(False)
 ax1.fill_between(years[:,0],ratio_mean + ratio_std,ratio_mean - ratio_std, color = [0.7,0.7,0.7])
 colors = iter(cm.rainbow(np.linspace(0,1,30)))
 for y in range(0,30):
     ax1.scatter(years[:,y],ratio_ann[:,y],color = next(colors))
 ax1.set_title('1980-2100')
-ax1.axis([1980,2100,0.5,1.0])
+ax1.axis([1980,2100,0.0,1.0])
 
 #1980-2030 subplot
 ax2 = plt.subplot(gs[1,:])
@@ -53,7 +60,7 @@ colors = iter(cm.rainbow(np.linspace(0,1,30)))
 for y in range(0,30):
     ax2.scatter(years[0:50,y],ratio_ann[0:50,y],color = next(colors))
 ax2.set_title('1980-2030')
-ax2.axis([1980,2030,0.75,0.95])
+ax2.axis([1980,2030,0.25,0.95])
 
 #2060-2100 subplot
 #ax3 = plt.subplot(gs[1,1])
@@ -65,6 +72,5 @@ ax2.axis([1980,2030,0.75,0.95])
 #ax3.set_title('2060-2100')
 #ax3.axis([2060,2100,0.6,3.1])
 
-plt.suptitle('Ratio of ice area:extent in 30 LENS members')
-#plt.show()
-plt.savefig('LENS_area_extent_ratio_21stcentury.png')
+plt.suptitle('Ratio of September ice area:extent in 30 LENS members')
+plt.savefig('LENS_area_extent_ratio_21stcentury_sept.png')
